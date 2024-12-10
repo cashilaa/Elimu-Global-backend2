9f5885795a5b77985d5daa24cb7d98c75e832e48
smartgreendeer <johnmuriithi7818@gmail.com>
2024-12-10 22:12:07 +0300

    Updated CORS configuration to allow frontend domain

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://elimu-global-testing.onrender.com', 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3001);
 }
