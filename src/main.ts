4e098b82936b422dcd267cffda14e268bff4d569
smartgreendeer <johnmuriithi7818@gmail.com>
2024-12-13 06:28:08 +0300

    "Fix CORS issue and update configuration"

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
-  
+
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://elimu-global-testing.onrender.com', // Specify the allowed origin
+    credentials: true, // Allow credentials
+  });
+
   await app.listen(port);
   console.log(`Application is running on port ${port}`);
 }
